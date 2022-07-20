c6882ae9e5bcc87aff2d47fbe96191a5c8ab6022
hjlee <hyojinlee@tpcg.co.kr>
2022-07-20 12:25:54 +0900

    test

---
+++ src/main/java/board/service/BoardService.java
@@ -0,0 +1,13 @@
+package board.service;
+
+import board.dto.BoardDto;
+import java.util.List;
+
+public interface BoardService {
+
+    List<BoardDto> selectBoardList() throws Exception;
+
+    void insertBoard(BoardDto board) throws Exception;
+
+    BoardDto selectBoardDetail(int boardIdx) throws Exception;
+}
